55f7ca598923636dd60fb3b8cf86a6d88ad7ca19
Sardor1009 <islomovsardor1009@gmail.com>
2023-12-09 09:23:24 +0500

    some

--- js/main.js
+++ js/main.js
@@ -301,3 +301,239 @@
 // chiqarButunSonlar(1, 3);
 
 // pr18
+
+// let son = 12;
+// let boluvchilar = [];
+// let yigindi = 0;
+
+// for (let i = 1; i <= son; i++) {
+//     if (son % i === 0) {
+//         boluvchilar.push(i);
+//         yigindi += i;
+//     }
+// }
+
+// console.log("Bo'luvchilar:", boluvchilar);
+// console.log("Bo'luvchilar soni:", boluvchilar.length);
+// console.log("Bo'luvchilar yig'indisi:", yigindi);
+
+
+// pr19
+
+// let n = 17; // Son kiriitng
+// let tub = true;
+
+// if (n <= 1) {
+//     console.log(`${n} soni tub emas.`);
+// } else {
+//     for (let i = 2; i <= Math.sqrt(n); i++) {
+//         if (n % i === 0) {
+//             tub = false;
+//             break;
+//         }
+//     }
+
+//     if (tub) {
+//         console.log(`${n} soni tub.`);
+//     } else {
+//         console.log(`${n} soni tub emas.`);
+//     }
+// }
+
+// pr20
+
+// let N = 5; // O'zgartiringan son
+
+// for (let i = 1; i <= N; i++) {
+//     let chiqar = "";
+//     for (let j = 1; j <= i; j++) {
+//         chiqar += j + " ";
+//     }
+//     console.log(chiqar.trim());
+// }
+
+
+// pr21
+
+// let A = 123;
+// let B = 5;
+
+// if(A > B){
+//   while (A >= B){
+//     A -= B;
+//   }
+//   console.log(A)
+// }
+// else{
+//     console.log("A kesma B dan kichik")
+// }
+
+// pr22
+
+
+// let A = 123;
+// let B = 5;
+// c = 0;
+
+// if(A > B){
+//   while (A >= B){
+//     A -= B;
+//     c++;
+//   }
+//   console.log(c)
+// }
+// else{
+//     console.log("A kesma B dan kichik")
+// }
+
+// pr23
+
+// let n = 28; // O'zgartiringan son
+
+// let daraja = 3;
+// let darajaSoni = 0;
+
+// while (n % daraja === 0) {
+//     n /= daraja;
+//     darajaSoni++;
+// }
+
+// let natija = darajaSoni > 0 ? "3 - ning darajasi" : "3 - ning darajasi emas";
+// console.log(natija);
+
+// pr24
+
+// `let A = 123;
+// let B = 5;
+// c = 0;
+
+// if(A > B){
+//   while (A >= B){
+//     A -= B;
+//     c++;
+//   }
+//   console.log("BUtun",  c)
+//   console.log("Qoldiq",A)
+// }
+// else{
+//     console.log("A kesma B dan kichik")
+// }`
+
+// pr25
+
+// let n = 12345; // O'zgartiringan son
+
+// let boLib = Math.floor(n);
+// let qoldiq = n % 1;
+// let teskariRaqamlar = "";
+
+// while (boLib > 0) {
+//     let raqam = boLib % 10;
+//     teskariRaqamlar += raqam;
+//     boLib = Math.floor(boLib / 10);
+// }
+
+// console.log("Bo'lib:", boLib);
+// console.log("Qoldiq:", qoldiq);
+// console.log("Teskari tartibdagi raqamlar:", teskariRaqamlar);
+
+// pr26
+
+// let n = 12345; // O'zgartiringan son
+
+// let boLib = Math.floor(n);
+// let qoldiq = n % 1;
+// let raqamlarYigindisi = 0;
+// let raqamlarSon = "";
+
+// while (boLib > 0) {
+//     let raqam = boLib % 10;
+//     raqamlarYigindisi += raqam;
+//     raqamlarSon = raqam + raqamlarSon;
+//     boLib = Math.floor(boLib / 10);
+// }
+
+
+// console.log("Raqamlar yig'indisi:", raqamlarYigindisi);
+// console.log("Raqamlar soni:", raqamlarSon);
+
+// pr27
+// let n = 12345; // O'zgartiringan son
+
+// let boLib = Math.floor(n);
+// let qoldiq = n % 1;
+// let borYoqliginiAniqlash = false;
+
+// while (boLib > 0) {
+//     let raqam = boLib % 10;
+//     if (raqam === 2) {
+//         borYoqliginiAniqlash = true;
+//         break;
+//     }
+//     boLib = Math.floor(boLib / 10);
+// }
+
+
+// console.log("2 raqami bor-yo'qligi:", borYoqliginiAniqlash);
+
+
+// pr28
+
+// let n = 12345; // O'zgartiringan son
+
+// let boLib = Math.floor(n);
+// let qoldiq = n % 1;
+// let toqRaqamlarBor = false;
+
+// while (boLib > 0) {
+//     let raqam = boLib % 10;
+//     if (raqam % 2 !== 0) {
+//         toqRaqamlarBor = true;
+//         break;
+//     }
+//     boLib = Math.floor(boLib / 10);
+// }
+
+// console.log("Toq raqamlar bor-yo'qligi:", toqRaqamlarBor);
+
+
+// pr29
+
+// let son = 1345431; // O'zgartiringan son
+
+// let sonStr = String(son);
+// let uzunlik = sonStr.length;
+// let yarimUzunlik = Math.floor(uzunlik / 2);
+// let palindromik = true;
+
+// for (let i = 0; i < yarimUzunlik; i++) {
+//     if (sonStr[i] !== sonStr[uzunlik - 1 - i]) {
+//         palindromik = false;
+//         break;
+//     }
+// }
+
+// console.log(palindromik)
+
+
+
+// pr30
+
+// let n = 17; // O'zgartiringan son
+
+// let tub = true;
+// let i = 2;
+
+// while (i <= Math.sqrt(n)) {
+//     if (n % i === 0) {
+//         tub = false;
+//         break;
+//     }
+//     i++;
+// }
+
+// if (tub && n > 1) {
+//     console.log(`${n} soni tub.`);
+// } else {
+//     console.log(`${n} soni tub emas.`);
+// }
